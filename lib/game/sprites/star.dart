9dbf15124a7e6e75e4ff51a1dbcb563e4a5bca62
ChocoPic <60088300+ChocoPic@users.noreply.github.com>
2023-10-25 15:26:36 +0900

    reset commit

---
+++ lib/game/sprites/star.dart
@@ -0,0 +1,50 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_avoid_game/game/sprites/bomb.dart';
+import 'package:flutter_avoid_game/game/sprites/player.dart';
+
+class Star extends RectangleComponent with HasGameRef, CollisionCallbacks{
+  static const starSize = 32.0;
+
+  Star(position)
+  :super(
+    position: position,
+    size: Vector2.all(starSize),
+    anchor: Anchor.bottomCenter,
+  );
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    paint.color = Colors.yellow;
+    add(RectangleHitbox());
+  }
+
+  @override
+  void update(double dt) {  //매 프레임마다 호출된다
+    super.update(dt);
+    position.y = position.y + 5;
+    if(position.y - size.y > gameRef.size.y){ //화면 벗어나면 제거
+      removeFromParent();
+    }
+  }
+
+  @override //충돌 처리 대상
+  bool onComponentTypeCheck(PositionComponent other) {
+    if(other is Star || other is Bomb){
+      return false;
+    }else{
+      return super.onComponentTypeCheck(other);
+    }
+  }
+
+  @override //충돌시
+  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if(other is Player){
+      removeFromParent();
+    }else{
+      super.onCollisionStart(intersectionPoints, other);
+    }
+  }
+}
